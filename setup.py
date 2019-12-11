cf9bbd9f471260dc0bdf6a9dad13db9a94a59c7d
Quentin Smith <quentin@mit.edu>
2019-12-11 15:23:08 -0500

    Switch from Pyrex to Cython

--- setup.py
+++ setup.py
@@ -2,24 +2,23 @@
 
 from setuptools import setup
 from distutils.extension import Extension
-from Pyrex.Distutils import build_ext
+from Cython.Build import cythonize
 
 setup(
     name="PyMoira",
-    version="4.3.0",
+    version="4.3.1",
     description="PyMoira - Python bindings for the Athena Moira library",
     author="Evan Broder",
     author_email="broder@mit.edu",
     license="MIT",
     py_modules=['moira'],
-    ext_modules=[
+    ext_modules=cythonize([
         Extension("_moira",
                   ["_moira.pyx"],
                   libraries=["moira", "krb5"]),
         Extension("mrclient",
                   ["mrclient.pyx"],
                   libraries=["mrclient", "moira"]),
-        ],
+    ]),
     scripts=['qy'],
-    cmdclass= {"build_ext": build_ext}
 )
